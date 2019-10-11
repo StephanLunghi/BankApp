b2d2f09350e6aeb951754b22bd511f7c4831f2bd
Stephan Lunghi <stlunghi@microsoft.com>
2019-10-11 12:12:46 -0400

    Intial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+
+    enum TypeOfAccount
+    {
+        Checking, 
+        Saving, 
+        CD, 
+        Loan
+
+
+    }
+
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+
+        #region Properties
+        public string AccountName { get; set; }
+        public decimal Balance { get; private set; }
+        public string EmailAddress { get; set; }
+        public TypeOfAccount AccountType { get; set; }
+
+        public int AccountNumber { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        #endregion
+
+        #region Constructor
+
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+
+            CreatedDate = DateTime.Now;
+
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+
+        }
+        /// <summary>
+        /// Withdraw money from your account
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object = instance of a class
+            var account = new Account
+            {
+                AccountType = TypeOfAccount.Checking,
+                EmailAddress = "test@test.com"
+            };
+            
+
+            account.Deposit(1023.45M);
+            Console.WriteLine($"AN: {account.AccountNumber}, EA:{account.EmailAddress}, B: {account.Balance}, AT: {account.AccountType}, CD: {account.CreatedDate}");
+
+
+
+            var account2 = new Account();
+            Console.WriteLine($"AN: {account2.AccountNumber}, EA:{account2.EmailAddress}, B: {account2.Balance}, AT: {account2.AccountType}, CD: {account2.CreatedDate}");
+
+
+        }
+    }
+}
